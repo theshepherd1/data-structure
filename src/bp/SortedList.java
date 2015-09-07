45157e89acb4b92e70e3f8a5a2ab4d4cc9710c84
Tony Mu <theveryshepherd@gmail.com>
2015-09-07 15:49:02 -0500

    sorted list

---
+++ src/bp/SortedList.java
@@ -0,0 +1,18 @@
+package bp;
+
+public class SortedList extends UnsortedList {
+	public final void insert(final int pValueToInsert) {\
+		// find where to insert into
+		// make sure not dup if not allowed
+		// push certain elements
+		//add value
+	}
+	
+	public void delete(final int pValueToDelete) {
+		//delete code here
+	}
+	
+	public final int find(final int pValueToFind) {
+		return -10;		
+	}
+}
