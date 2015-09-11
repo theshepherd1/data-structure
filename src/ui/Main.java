fdce9235e0b27916d6349d8da5a030124ae13230
unknown <166636@PLSLAB18084.cofo.edu>
2015-09-11 15:52:06 -0500

    works on main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,15 @@
 package ui;
 
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Data myData = new Data(5,2);
+		System.out.println(myData);
+		Stack myStack = new Stack();
+		myStack.push(myData);
+		System.out.println(myStack);
 		
 	}
 
