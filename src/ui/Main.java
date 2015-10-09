57b8ff0d97f5a8f58b937b667cb381bf2381c0a8
tonymu <tony.mu101999@gmail.com>
2015-10-08 23:22:28 -0500

    finished hash function

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -10,43 +10,25 @@ import bp.Stack;
 import bp.Link;
 import bp.LinkedList;
 import bp.Node;
+import bp.Hash;
+
 
 //import bp.Data;
 
 public class Main {
 
 	public static void main(String[] args) {
-		BinaryTree n = new BinaryTree();
-		
-		LocalDate[] dates = new LocalDate[10];
-		for (int i = 1; i < 11; i ++) {
-			dates[i-1] = LocalDate.of(2000 + i * (-1)^i, 2, 3);
-		}
-		
-		for (LocalDate date : dates) {
-			System.out.println(date);
-			n.insert(date);
-		}
-		
-//		
-		for (int i = 1; i < 11; i ++) {
-//			System.out.println(dates[i-1]);
-			System.out.println(n.find(dates[i-1]));
-		}
-		
-		System.out.println(n.getSize());
+        System.out.println(Hash.hash("a"));
+        System.out.println(Hash.hash("b"));
+        System.out.println(Hash.hash("aa"));
+        System.out.println(Hash.hash(""));
+        
+        System.out.println(Hash.hash("aaaaa"));
+        System.out.println(Hash.hash("s"));
+        System.out.println(Hash.hash("311"));
+        System.out.println(Hash.hash("3"));
+        System.out.println(Hash.hash("1"));
 
-		
-		System.out.println(n.getMaxHeight(n.getRootNode()));
-		
-		n.delete(dates[3]);
-		n.delete(dates[4]);
-		n.delete(dates[4]);
-		n.delete(dates[4]);
-		n.delete(dates[4]);
 
-		System.out.println(n.getSize());
-//		System.out.println(n.toString());
-		
 	}
 }
