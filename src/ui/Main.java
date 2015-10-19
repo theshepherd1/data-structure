3d5b35a7d188766a3d3361258a67e965d8392413
unknown <166636@PLSLAB18090.cofo.edu>
2015-10-19 15:51:15 -0500

    main class code

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,86 +1,31 @@
 
 package ui;
 
+import bp.Vertex;
+import bp.Edge;
+import bp.Graph;
 import bp.UnsortedList;
 
 public class Main {
 
 public static void main(String[] args) {
-UnsortedList ul = new UnsortedList();
 
-//allowing dups
+	Graph myGraph = new Graph();
+	Vertex a = new Vertex('A');
+	Vertex b = new Vertex('B');
+	Vertex c = new Vertex('C');
+	Vertex d = new Vertex('D');
+	Vertex e = new Vertex('E');
 
-ul.setDuplicatesAllowed(true);
+	myGraph.addEdge(new Edge(a,b));
+	myGraph.addEdge(new Edge(b,c));
+	myGraph.addEdge(new Edge(d,e));
 
-//10 points
-ul.initializeWithRandomData(5);
-System.out.println("Should have five random elements with dups potentially:");
-System.out.println(ul);
-
-//10 points
-ul.bubbleSort();
-System.out.println("Should be sorted:");
-System.out.println(ul);
-
-//10 points
-System.out.println("Should have removed the first '5' if applicable");
-ul.delete(5);
-System.out.println(ul);
-
-//10 points
-System.out.println("Should insert two 10s");
-ul.insert(10);
-ul.insert(10);
-System.out.println(ul);
-
-//10 points
-System.out.println("Should delete all 10s");
-ul.deleteAll(10);
-System.out.println(ul);
-
-//10 points
-ul.clear();
-System.out.println("Should be empty");
-System.out.println(ul);
-
-//10 points
-System.out.println("Should still be empty -- tried to delete from an empty list");
-ul.delete(5);
-System.out.println(ul);
-
-//No dups allowed
-
-//10 points
-ul = new UnsortedList();
-ul.setDuplicatesAllowed(false);
-ul.initializeWithRandomData(10);
-System.out.println("Should have 10 random elements without dups");
-System.out.println(ul);
-
-//10 points
-ul.insert(5);
-System.out.println("Should silently refuse to insert a 5 (NOTE TO SELF: make sure MAX_SIZE = 10)"); 
-System.out.println(ul);
-
-//10 points
-ul.delete(5);
-System.out.println("Should remove the five if applicable");
-System.out.println(ul);
-
-//10 points
-ul.deleteAll(6);
-System.out.println("Should remove all sixes");
-System.out.println(ul);
-
-//10 points
-ul.insert(5);
-System.out.println("Should insert a five if room");
-System.out.println(ul);
-
-//10 points
-ul.selectionSort();
-System.out.println("Should be sorted");
-System.out.println(ul);
+	System.out.println(myGraph.isConnected());
+	System.out.println(myGraph.isAdjacent('A', 'B'));
+	System.out.println(myGraph.isAdjacent('C', 'B'));
+	System.out.println(myGraph.isAdjacent('A', 'C'));
+	System.out.println(myGraph.getAdjacentVertices('B'));
 
 }
 
